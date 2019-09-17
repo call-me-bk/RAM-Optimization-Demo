c517b2b1a4b3209cef228e865514d718175b10ab
Irfan <irfansa1@ymail.com>
2019-09-17 18:25:09 +0530

    Initial commit

---
+++ app/src/main/java/oncreate/apps/ramoptimizationdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package oncreate.apps.ramoptimizationdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void showList(View v){
+
+    }
+
+    public void showRecycler(View v){
+        
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
